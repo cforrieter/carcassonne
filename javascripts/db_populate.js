3bda793ae3087f302225f93a60f821ce661948ce
freemer <pushpaw@gmail.com>
2015-10-15 23:30:38 +0000

    Slight changes to data structure

--- javascripts/db_populate.js
+++ javascripts/db_populate.js
@@ -11,71 +11,68 @@ MongoClient.connect(url, function (err, db) {
     collection.remove({});
     var tile1 = {
       'type':'L',
-      'top':'road', 
-      'right':'city', 
-      'bottom':'road',
-      'left':'road',
-      'center':'terminus',
+      'top':{'conditon':'road','meeple':'player1'}, 
+      'right':{'condition':'city'}, 
+      'bottom':{'condition':'road'},
+      'left':{'condition':'road'},
+      'center'{'condition':'terminus'},
       'banner':false,
-      'position':{'x':0,'y':0},
-      'meeple':{'player':1,'position':2}
+      'position':{'x':0,'y':0}
     };
     var tile2 = {
-      'type':'V',
-      'top':'field', 
-      'right':'field', 
-      'bottom':'road',
-      'left':'road',
-      'center':'road',
-      'banner':false,
-      'coords':null,
-      'meeple':null
-    };
-    var tile3 = {
-      'type':'E',
-      'top':'city', 
-      'right':'field', 
-      'bottom':'field',
-      'left':'field',
-      'center':'field',
-      'banner':false,
-      'position':null,
-      'meeple':null
-    };
-    var tile4 = {
       'type':'L',
-      'top':'road', 
-      'right':'city', 
-      'bottom':'road',
-      'left':'road',
-      'center':'terminus',
+      'top':{'conditon':'road'}, 
+      'right':{'condition':'city'}, 
+      'bottom':{'condition':'road'},
+      'left':{'condition':'road'},
+      'center'{'condition':'terminus'},
       'banner':false,
       'position':{'x':0,'y':1},
-      'meeple':null
     };
-    var tile5 = {
-      'type':'V',
-      'top':'field', 
-      'right':'field', 
-      'bottom':'road',
-      'left':'road',
-      'center':'road',
-      'banner':false,
-      'position':null,
-      'meeple':null
-    };
-    var tile6 = {
-      'type':'E',
-      'top':'city', 
-      'right':'field', 
-      'bottom':'field',
-      'left':'field',
-      'center':'field',
-      'banner':false,
-      'position':null,
-      'meeple':null
-    };
-    collection.insert([tile1, tile4], function (err, result) {
+    // var tile2 = {
+    //   'type':'V',
+    //   'top':'field', 
+    //   'right':'field', 
+    //   'bottom':'road',
+    //   'left':'road',
+    //   'center':'road',
+    //   'banner':false,
+    //   'position':null,
+    // };
+    // var tile3 = {
+    //   'type':'E',
+    //   'top':'city', 
+    //   'right':'field', 
+    //   'bottom':'field',
+    //   'left':'field',
+    //   'center':'field',
+    //   'banner':false,
+    //   'position':null,
+    //   'meeple':null
+    // };
+    // var tile5 = {
+    //   'type':'V',
+    //   'top':'field', 
+    //   'right':'field', 
+    //   'bottom':'road',
+    //   'left':'road',
+    //   'center':'road',
+    //   'banner':false,
+    //   'position':null,
+    //   'meeple':null
+    // };
+    // var tile6 = {
+    //   'type':'E',
+    //   'top':'city', 
+    //   'right':'field', 
+    //   'bottom':'field',
+    //   'left':'field',
+    //   'center':'field',
+    //   'banner':false,
+    //   'position':null,
+    //   'meeple':null
+    // };
+    collection.insert([tile1, tile2], function (err, result) {
       if (err) {
         console.log('Error:', err);
       } else {
@@ -83,5 +80,11 @@ MongoClient.connect(url, function (err, db) {
         ' The documents inserted with "_id" are:', result.length, result);
       }
     })
+
+    var cursor = collection.find();
+    cursor.toArray(function(err, tiles){
+      console.log(tiles);
+    })
   }
 })
+
