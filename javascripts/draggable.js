61d62c07a67d78325d6043e5cef305c3261ebd6c
Warren Wegner <warrenwegner@gmail.com>
2015-10-16 12:01:34 -0700

    created confirm/decline options on tile drop

--- javascripts/draggable.js
+++ javascripts/draggable.js
@@ -11,39 +11,7 @@ function Draggable(game, x, y, key, frame)
 Draggable.constructor = Draggable;
 Draggable.prototype = Object.create(Phaser.Sprite.prototype);
 
-Draggable.prototype.onClick = function onClick(draggable, pointer)
-{
-  this.currentPointer = pointer;
-  if(this.dragged)
-  {
-    // Stop dragging
-    console.log("Dropped", this);
 
-    var target = { x: Math.floor((this.x + 45) / 90) * 90,
-                   y: Math.floor((this.y + 45) / 90) * 90
-                 };
-    this.game.add.tween(this).to(target, 250).start();
-  }
-  else
-  {
-    // Start dragging
-    console.log("Grabbed", this);
-    this.fixedToCamera = false;
-  }
-  this.dragged = !this.dragged;
-}
-
-Draggable.prototype.update = function update()
-{
-  if(this.dragged && this.currentPointer)
-  {
-    // console.log(this.currentPointer.worldX, this.currentPointer.worldY);
-    this.x = this.currentPointer.worldX;
-    this.y = this.currentPointer.worldY;
-
-
-  }
-}
 
 
 function Tile(game, x, y, frame)
@@ -56,3 +24,62 @@ function Tile(game, x, y, frame)
 Tile.constructor = Tile;
 Tile.prototype = Object.create(Draggable.prototype);
 
+Tile.prototype.onClick = function onClick(draggable, pointer)
+{
+  this.currentPointer = pointer;
+  if(this.dragged)
+  {
+    // Stop dragging
+
+    var target = { x: Math.floor((this.x + 45) / 90) * 90,
+                   y: Math.floor((this.y + 45) / 90) * 90
+                   };
+    this.game.add.tween(this).to(target, 250).start();
+
+    confirmDrop(target, function(confirmed){
+      if (confirmed) {
+        console.log("Dropped", tile);
+        tile.inputEnabled = false;
+      } 
+    }, this);
+
+
+  }
+  else
+  {
+    // Start dragging
+    console.log("Grabbed", this);
+    this.fixedToCamera = false;
+  }
+  this.dragged = !this.dragged;
+
+  function confirmDrop(target, callback) {
+
+    var confirm = tile.game.add.button(target.x + 45, target.y - 45, 'tiles', confirm, this, 23, 23, 23);
+    var decline = tile.game.add.button(target.x - 135, target.y - 45, 'tiles', decline, this, 22, 22, 22);
+
+    function confirm() {
+      confirm.destroy();
+      decline.destroy();
+      callback(true);
+    }
+
+    function decline() {
+      confirm.destroy();
+      decline.destroy();
+      callback(false);
+    }
+  }
+}
+
+Tile.prototype.update = function update()
+{
+  if(this.dragged && this.currentPointer)
+  {
+    // console.log(this.currentPointer.worldX, this.currentPointer.worldY);
+    this.x = this.currentPointer.worldX;
+    this.y = this.currentPointer.worldY;
+
+
+  }
+}
