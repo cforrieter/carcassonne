c661d8cdfb453636a312daa05e034eb64e51943d
malisbad <matt1256@gmail.com>
2015-10-20 15:44:49 -0700

    Needed to add green rupee sprite sheet and main title background
    Animations confirmed to work, background added

--- javascripts/mainMenu.js
+++ javascripts/mainMenu.js
@@ -10,23 +10,32 @@ CarcassoneGame.mainMenu = function(game) {
 CarcassoneGame.mainMenu.prototype = {
 
   preload: function() {
+    game.load.spritesheet('green-rupee', 'assets/zelda-green-rupee-spritesheet.png',8,14,3);
     game.load.image('meeple-3d-blue', 'assets/meeple-3d-blue.png');
-    game.load.image('meeple-banner', 'assets/meeple-all-flat.png');
     game.load.image('header', 'assets/carcassone-header.png');
     game.load.image('meeple-blue-flat', 'assets/meeple-blue-flat.png');
+    game.load.image('background', 'assets/zelda-opening-background.png');
   },
 
   create: function() {
+    var background = game.add.sprite(0,0, 'background');
+
+    var meep = game.add.sprite(game.world.centerX, game.world.centerY, 'green-rupee');
+    meep.anchor.set(0.5);
+    meep.inputEnabled = true;
+    var sparkle = meep.animations.add('sparkle');
+    meep.animations.play('sparkle', 4, true);
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     emitter = game.add.emitter(0, 0, 100);
-    emitter.makeParticles('meeple-3d-blue');
+    emitter.makeParticles('green-rupee');
+    emitter.forEach(function(singleParticle) {
+    singleParticle.animations.add('particleAnim');
+    singleParticle.animations.play('particleAnim', 3, true);
+    });
     emitter.gravity = 200;
-
-    var meep = game.add.sprite(game.world.centerX, game.world.centerY, 'meeple-3d-blue');
-    meep.anchor.set(0.5);
-    meep.inputEnabled = true;
-    meep.scale.setTo(0.20,0.20);
+    // meep.scale.setTo(0.20,0.20);
+    
     var header = game.add.sprite(10, 32, 'header');
     meep.events.onInputDown.add(function() {
     this.state.start('mainGame')}, this);
