1aed525cef5028b96d9344546d60d303b9cdf4b4
malisbad <matt1256@gmail.com>
2015-10-20 16:13:39 -0700

    Needed to add red and blue spritesheets

--- javascripts/mainMenu.js
+++ javascripts/mainMenu.js
@@ -10,6 +10,8 @@ CarcassoneGame.mainMenu = function(game) {
 CarcassoneGame.mainMenu.prototype = {
 
   preload: function() {
+    game.load.spritesheet('red-rupee', 'assets/zelda-red-rupee-spritesheet.png',8,14,3);
+    game.load.spritesheet('blue-rupee', 'assets/zelda-blue-rupee-spritesheet.png',8,14,3);
     game.load.spritesheet('green-rupee', 'assets/zelda-green-rupee-spritesheet.png',8,14,3);
     game.load.image('meeple-3d-blue', 'assets/meeple-3d-blue.png');
     game.load.image('header', 'assets/carcassone-header.png');
@@ -18,7 +20,9 @@ CarcassoneGame.mainMenu.prototype = {
   },
 
   create: function() {
+
     var background = game.add.sprite(0,0, 'background');
+    var header = game.add.sprite(10, 32, 'header');
 
     var meep = game.add.sprite(game.world.centerX, game.world.centerY, 'green-rupee');
     meep.anchor.set(0.5);
@@ -28,15 +32,15 @@ CarcassoneGame.mainMenu.prototype = {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     emitter = game.add.emitter(0, 0, 100);
-    emitter.makeParticles('green-rupee');
+    emitter.makeParticles('red-rupee');
     emitter.forEach(function(singleParticle) {
     singleParticle.animations.add('particleAnim');
-    singleParticle.animations.play('particleAnim', 3, true);
+    singleParticle.animations.play('particleAnim', 4, true);
     });
     emitter.gravity = 200;
     // meep.scale.setTo(0.20,0.20);
     
-    var header = game.add.sprite(10, 32, 'header');
+    
     meep.events.onInputDown.add(function() {
     this.state.start('mainGame')}, this);
     game.input.onDown.add(this.particleBurst, this);
