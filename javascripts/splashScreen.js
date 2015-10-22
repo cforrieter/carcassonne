d73673ed17fb3ba7a4b651778075460ee403efc6
malisbad <matt1256@gmail.com>
2015-10-22 08:42:53 -0700

    Needed to add SFX assets and LHL/Node logos

--- javascripts/splashScreen.js
+++ javascripts/splashScreen.js
@@ -2,13 +2,22 @@
 
 CarcassoneGame.splashScreen = function(game) {
   this.phaserLogo;
+  this.stateSwapTimer
 };
+
+var stateSwapTimer;
+var bmd;
+var area;
+var dropTime = 0;
 
 CarcassoneGame.splashScreen.prototype = {
 
   preload: function() {
     game.load.image('space', 'assets/starfield.png');
     game.load.image('phaserLogo', 'assets/phaser2.png');
+    game.load.image('lhl-logo', 'assets/lhl-logo.png');
+    game.load.image('node-logo', 'assets/node-logo.png');
+    game.load.audio('secret', 'assets/secret.mp3');
   },
 
   create:  function() {
@@ -21,17 +30,46 @@ CarcassoneGame.splashScreen.prototype = {
     tween.start();
     tween.onComplete.add(this.introTimer, this);
     game.input.onDown.add(this.goToZelda, this);
+
+    var lhl = game.add.sprite(20, 550, 'lhl-logo');
+    lhl.scale.setTo(0.5, 0.5);
+
+    var node = game.add.sprite(575, 500, 'node-logo');
+    node.scale.setTo(0.5,0.5);
+
+
+    bmd = game.make.bitmapData(800,600);
+    bmd.addToWorld();
+    area = new Phaser.Rectangle(20, 550, 400, 16);
+    bmd.copyRect('lhl-logo', area, 20, 550);
+
   },
 
   goToZelda: function () {
-    tween.
+    var navi = this.game.add.audio('secret');
+    navi.allowMultiple = true;
+    navi.addMarker('secret', 0, 2);
+    navi.play('secret');
+    this.game.time.events.remove(stateSwapTimer);
   },
 
   introTimer: function() {
-    this.game.time.events.add(10000, this.goToMainMenu, this);
+    stateSwapTimer = this.game.time.events.add(10000, this.goToMainMenu, this);
   },
 
   goToMainMenu: function() {
     this.state.start('mainMenu');
+  },
+
+  update: function() {
+    if (area.y > 0 && game.time.now > dropTime) {
+      for (var y = 0; y < area.y; y++)
+      {
+        bmd.copyRect('lhl-logo', area, 20, y);
+      }
+
+      area.y--;
+      dropTime = game.time.now + 25;
+    }
   }
 };
